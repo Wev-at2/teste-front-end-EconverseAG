052068f5ff5e6e08f1df640708c020b70e63829d
Weverton Costa <weverton.costa@buscacliente.com.br>
2026-04-29 11:38:28 -0300

    feat: add react plugin and update proxy configuration for product API endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://app.econverse.com.br',
+      '/api/produtos': {
+        target: 'https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos',
         changeOrigin: true,
-        rewrite: (path) =>
-          path.replace(
-            '/api',
-            '/teste-front-end/junior/tecnologia/lista-produtos'
-          ),
-      },
-    },
-  },
-});
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api/produtos': {
-//         target: 'https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api\/produtos/, '/produtos.json'),
-//       }
-//     }
-//   }
-// })
+        rewrite: (path) => path.replace(/^\/api\/produtos/, '/produtos.json'),
+      }
+    }
+  }
+})
